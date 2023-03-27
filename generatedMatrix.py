b3cd3ee6dfe113c4ac431f64ef72e673cdf6103d
GIMBERT Vincent <vincent.gimbert@yahoo.fr>
2023-03-27 11:03:18 +0200

    list_of_hexa added

--- generatedMatrix.py
+++ generatedMatrix.py
@@ -1,1 +1,10 @@
+def hexa_for_each_char(message):
+    list_hex = []
+    for lettre in message:
+        list_hex.append(format(ord(lettre), "x"))
+    return list_hex
 
+def matrice(list_hexa):
+    # TODO
+    pass
+hexa_for_each_char("On vient pas de la street ?")
